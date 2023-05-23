4842ad788c7de94dc7bddadbe40ea7d05a0ebf12
Mahammad V. Mustafayev <mahammadvm@code.local>
2023-05-23 09:09:18 +0400

    added ef core test for excel

---
+++ excelcore/excelcore/DAL/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿
+
+using Microsoft.EntityFrameworkCore;
+
+namespace excelcore.DAL;
+
+internal class AppDbContext:DbContext
+{
+    protected override void OnConfiguring(DbContextOptionsBuilder options)
+    {
+        options.UseSqlServer(@"Server=RUBIS09\SQLEXPRESS;Database=Report;Trusted_Connection=True;");
+    }
+}
---
+++ excelcore/excelcore/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace excelcore;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        
+    }
+}
